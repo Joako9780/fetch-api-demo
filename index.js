d2420cdb2ca9bb448d01962d81dc0996869476b5
Joako9780 <57472877+Joako9780@users.noreply.github.com>
2025-10-09 11:34:09 -0300

    Update index.js
    Creación de función para obtener valores random entre 1 y 20

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const IMAGE_URL = 'https://cdn.thesimpsonsapi.com/500';
  * @returns {Promise<Object>}  // El objeto que retorna es una promesa que se resuelve a un objeto
  */
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; // El max y el min son inclusivos
+}
+
 const fetchCharacter = async (randomValue) => {  //id = 1 es un default value por si id no recibe ningun valor
     const response = await fetch(`https://thesimpsonsapi.com/api/characters/${randomValue}`);  // String interpolation, con lo cual se puede escapar algo, y se pide que se resuelva a algo que puede ser resuelto en primitico
     const character = await response.json();
@@ -27,12 +33,7 @@ const fetchCharacter = async (randomValue) => {  //id = 1 es un default value po
     buttonElement.addEventListener('click', async () => {
         buttonElement.disabled = true; // Deshabilitar el boton
         spinnerElement.style.display = 'block'; // Mostrar el spinner
-
-        let randomValue = 0;
-        do {
-           randomValue = Math.floor(Math.random() * 100) + 1; // 1 - 100
-        } while (randomValue > 20); // Asi nos aseguramos que el id este entre 1 y 20, porque solo hay 20 personajes
-
+        let randomValue = getRandomInt(1, 20);
         const character = await fetchCharacter(randomValue);
         spinnerElement.style.display = 'none'; // Ocultar el spinner
         characterElement.textContent = JSON.stringify(character, null, 2); // El null y el 2 son para que se vea bonito el JSON, 2 es el nivel de indentacion
